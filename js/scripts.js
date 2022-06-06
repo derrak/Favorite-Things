e50731f42a0e4506d7b7e972cee17ab3c7fa440c
danielware <waredanielb@gmail.com>
2022-06-06 11:06:09 -0700

    Co-authored-by: Derrak <derrak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,12 @@ $(document).ready(function() {
     const fav3 = $("#favorites3").val();
 
     const favArray = [fav1, fav2, fav3];
-    console.log(favArray);
-    console.log(favArray[1],favArray[0],favArray[2]);
+    const newArray = [];
+    newArray.push(favArray[1],favArray[0],favArray[2]);
+
+    $(".favorite2").append(newArray[0]);
+    $(".favorite1").append(newArray[1]);
+    $(".favorite3").append(newArray[2]);
   });
-
-
 }); 
 
