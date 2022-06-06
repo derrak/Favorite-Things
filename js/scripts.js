5c4e850560ef1027e6d955ab25c9faaef35b6bf3
danielware <waredanielb@gmail.com>
2022-06-06 10:50:10 -0700

    Added array functionality.
    Co-authored-by: Derrak <derrak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+  $("#form").submit(function(event){
+    event.preventDefault();
+    const fav1 = $("#favorites1").val();
+    const fav2 = $("#favorites2").val();
+    const fav3 = $("#favorites3").val();
+
+    const favArray = [fav1, fav2, fav3];
+    console.log(favArray);
+    console.log(favArray[1],favArray[0],favArray[2]);
+  });
+
 
 }); 
 
